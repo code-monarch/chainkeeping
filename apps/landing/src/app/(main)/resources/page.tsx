7b4f13017e832916e459d00d0cb1a7dd450f9de5
code-monarch <adiejoel14+monarch@gmail.com>
2025-01-11 15:32:21 +0100

    feat: Implement Terms of Service

--- apps/landing/src/app/(main)/resources/page.tsx
+++ apps/landing/src/app/(main)/resources/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+
 import React from 'react'
+import JoinUsWidget from '@/pattern/home/templates/join-us-widget'
+import SupportHeroSection from '@/pattern/resources/templates/support-hero-section'
+import SupportTemp from '@/pattern/resources/templates/support-temp'
 
 const ResourcesPage = () => {
   return (
-    <div>ResourcesPage</div>
+    <div className='w-full flex flex-col items-center gap-y-[88px] mb-[144px]'>
+      <SupportHeroSection />
+      <SupportTemp />
+      <JoinUsWidget />
+    </div>
   )
 }
 
---
+++ apps/landing/src/app/(main)/resources/terms-of-use/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import TermsOfUseTemp from '@/pattern/resources/templates/terms-of-use-temp';
+import React from 'react'
+
+const TermsOfUsePage = () => {
+  return (
+      <div className='w-full h-full md:container flex items-start justify-center pt-14 mb-[144px]'>
+        <TermsOfUseTemp />
+    </div>
+  )
+}
+
+export default TermsOfUsePage
