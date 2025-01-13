3b054b06215efbef06faf56a2177261da4b69c04
code-monarch <adiejoel14+monarch@gmail.com>
2025-01-14 00:14:19 +0100

    feat: Make FAQs mobile reponsiveness

--- apps/landing/src/pattern/resources/templates/FAQs-temp.tsx
+++ apps/landing/src/pattern/resources/templates/FAQs-temp.tsx
@@ -6,8 +6,8 @@ import FaqsNavigation from './faqs-navigation'
 
 const FAQsTemp = () => {
     return (
-        <div className='relative w-full min-h-[500px] h-fit mb-[153px]'>
-            <div className='w-full lg:container flex items-start justify-between gap-16'>
+        <div className='relative w-full min-h-[500px] h-fit mb-12 lg:mb-[153px]'>
+            <div className='w-full flex items-start justify-between gap-16 px-[18px] lg:container'>
                 {/* left */}
                 <div className='w-full flex flex-col gap-y-16'>
                     <div id="general" className='w-full flex flex-col gap-y-4'>
@@ -36,7 +36,7 @@ const FAQsTemp = () => {
                 </div>
 
                 {/* Right */}
-                <div className='sticky top-[88px] right-0 w-[184px] min-h-[203px]'>
+                <div className='hidden lg:block sticky top-[88px] right-0 w-[184px] min-h-[203px]'>
                     <h6 className='w-full font-rubik font-medium text-base text-foreground pb-3'>On this page</h6>
                     <FaqsNavigation />
                 </div>
