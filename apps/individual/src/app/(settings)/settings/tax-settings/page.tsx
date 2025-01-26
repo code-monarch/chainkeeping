d3058ba8d2bd5c4e86f78f98b81208e4f81cac61
Cygnux X-1 <62252129+Dekatron322@users.noreply.github.com>
2025-01-26 16:12:53 +0100

    tax setting

---
+++ apps/individual/src/app/(settings)/settings/tax-settings/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import AccountTransactionSection from "@/pattern/accounts/templates/account-section";
+import ChangePasswordSettings from "@/pattern/settings/templates/change-password";
+import ProfileSettings from "@/pattern/settings/templates/profiles-settings";
+import TaxSettingsSettings from "@/pattern/settings/templates/tax-settings";
+import { Sidebar, SidebarProvider } from "@chainkeeping/ui";
+
+const ChangePassword = () => {
+	return (
+		<SidebarProvider>
+			<div className='w-full min-h-full h-fit flex flex-col gap-y-[144px] mb-[144px]'>
+				<TaxSettingsSettings />
+			</div>
+		</SidebarProvider>
+	);
+};
+
+export default ChangePassword;
